73db1a7e97c8beb62399c5972d4c6d1ecd2a1fca
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-06-15 20:20:47 +0300

    Factory method - demo

--- Creational/FactoryMethod/Program.cs
+++ Creational/FactoryMethod/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace FactoryMethod
 {
@@ -6,7 +7,26 @@ namespace FactoryMethod
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			PlayerCreator playerFactory;
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				playerFactory = new WindowsPlayerCreator();
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				playerFactory = new LinuxPlayerCreator();
+			}
+			else
+			{
+				throw new Exception("Only Linux and Windows operating systems are supported.");
+			}
+
+			Console.WriteLine("Please specify the path to the file to play");
+			var filePath = Console.ReadLine() ?? string.Empty;
+			playerFactory.CreatePlayer().Play(filePath);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
 	}
 }
