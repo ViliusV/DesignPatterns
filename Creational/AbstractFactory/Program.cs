d597df60e80738ad51787ef78e4eaf13d004d296
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-06-16 20:15:06 +0300

    Abstract Factory - add demo code

--- Creational/AbstractFactory/Program.cs
+++ Creational/AbstractFactory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace AbstractFactory
 {
@@ -6,7 +7,33 @@ namespace AbstractFactory
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
+            PlayerCreator? playerFactory;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                playerFactory = new WindowsPlayerCreator();
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                playerFactory = new LinuxPlayerCreator();
+            else
+                throw new Exception("Only Linux and Windows operating systems are supported.");
+
+            Console.WriteLine("Please specify the path to the file to play.");
+
+            var filePath = Console.ReadLine() ?? string.Empty;
+            playerFactory.CreatePlayButton().Play(filePath);
+
+            Console.WriteLine("Playing audio. Type 'stop' to stop it or 'exit' to exit the application.");
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "stop")
+                    playerFactory.CreateStopButton().Stop(filePath);
+                else if (command == "exit")
+                    break;
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
 	}
 }
