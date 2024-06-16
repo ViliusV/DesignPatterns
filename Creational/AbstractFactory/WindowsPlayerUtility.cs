76ca247b4cec3df6419dbec8b829aadca0bbabab
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-06-16 20:05:22 +0300

    Abstract Factory - initial code

---
+++ Creational/AbstractFactory/WindowsPlayerUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AbstractFactory
+{
+	internal static class WindowsPlayerUtility
+	{
+		[DllImport("winmm.dll")]
+		private static extern int mciSendString(string command, StringBuilder stringReturn, int returnLength, IntPtr hwndCallback);
+		public static void ExecuteMciCommand(string commandString)
+		{
+			var sb = new StringBuilder();
+			var result = mciSendString(commandString, sb, 1024 * 1024, IntPtr.Zero);
+			Console.WriteLine(result);
+		}
+	}
+}
