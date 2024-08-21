7d1f25dca6a9879005eaa5b0caf73630312a7a30
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-08-21 12:54:28 +0300

    Singleton - implement pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var object1 = SingletonObject.GetInstance();
+			Console.WriteLine($"Data of object 1: {object1.Data}");
+
+			var object2 = SingletonObject.GetInstance();
+			Console.WriteLine($"Data of object 2: {object2.Data}");
+			Console.WriteLine($"Are objects equal? {object.Equals(object1, object2)}");
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Creational/Singleton/SingletonObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+	internal class SingletonObject
+	{
+		private static SingletonObject? instance;
+
+		public int Data
+		{
+			get; private set;
+		}
+
+		private SingletonObject(int data)
+		{
+			Data = data;
+		}
+
+		public static SingletonObject GetInstance() {
+			if (instance is null)
+			{
+				var random = new Random();
+				instance = new SingletonObject(random.Next());
+			}
+
+			return instance;
+		}
+	}
+}
