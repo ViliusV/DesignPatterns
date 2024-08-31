716df630221f18672f99c20d1871387a530a1dbf
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-08-31 20:36:15 +0300

    Flyweight - implemented pattern

---
+++ Structural/Flyweight/SoldierFlyweight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Flyweight
+{
+	internal class SoldierFlyweight
+    {
+        public SoldierFlyweight(string soldierType, int experienceLevel, int speed, int strength)
+        {
+            SoldierType = soldierType;
+            ExperienceLevel = experienceLevel;
+            Speed = speed;
+            Strength = strength;
+        }
+
+        public string SoldierType { get; private set; }
+        public int ExperienceLevel { get; private set; }
+        public int Speed { get; private set; }
+        public int Strength { get; private set; }
+
+        public void Eliminate(int soldierId)
+        {
+            Console.WriteLine($"{SoldierType} {soldierId} has been eliminated.");
+        }
+    }
+}
