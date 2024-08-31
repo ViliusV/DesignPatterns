2abb5e5a83d41efe336ab482243be155b42d0a2a
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-08-31 22:01:43 +0300

    Revert "Revert "Command - implemented pattern""
    This reverts commit 616f7b4a5e895a6281804679a719d9e9c5c93dc2.

---
+++ Behavioral/ChainOfResponsibility/RequestHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainOfResponsibility
+{
+	internal abstract class RequestHandler
+    {
+        protected RequestHandler successor;
+
+        public abstract Response HandleRequest(Request request);
+
+        public void SetNext(RequestHandler successor)
+        {
+            this.successor = successor;
+        }
+    }
+}
