88fbde0daeeaa187f81c60a5475379aa233613e7
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-08-31 21:59:36 +0300

    Revert "Chain of Responsibility - implemented pattern"
    This reverts commit e0ddec6452b234c2357052f3a491a360c31f59ee.

--- Behavioral/ChainOfResponsibility/Request.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChainOfResponsibility
-{
-	internal class Request
-	{
-		public string Username { get; set; }
-		public string Password { get; set; }
-		public string Role { get; set; }
-	}
-}
