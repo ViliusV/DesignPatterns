e0ddec6452b234c2357052f3a491a360c31f59ee
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2024-08-31 21:58:30 +0300

    Chain of Responsibility - implemented pattern

---
+++ Behavioral/ChainOfResponsibility/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace ChainOfResponsibility
+{
+	internal class Request
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+}
---
+++ Behavioral/ChainOfResponsibility/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainOfResponsibility
+{
+	internal class Response
+    {
+        public bool Success { get; }
+        public string Message { get; }
+
+        public Response(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
